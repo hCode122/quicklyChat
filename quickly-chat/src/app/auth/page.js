bb764c9ee33925cb7f05c9ca542e0a06b10a51c9
hCode122 <hCode122@protonmail.me>
2024-02-19 19:26:45 +0300

    created login/signup functions

--- quickly-chat/src/app/auth/page.js
+++ quickly-chat/src/app/auth/page.js
@@ -2,13 +2,48 @@
 import { useRouter } from 'next/navigation'
 import { useState } from 'react';
 import Link from 'next/link';
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { NextResponse } from 'next/server';
+import { useEffect } from "react"
+
 
 const Login = () => {
+    const router = useRouter();
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const {dispatch,user} = useAuthContext();
+    useEffect(() => {if (user != null) {
+        router.push("/")
+        }
+    }, [user])
 
+    async function onSubmit(event) {
+        event.preventDefault();
+        try {
+            await fetch("http://localhost:3001/api/auth/login",{
+                method: "POST"
+            ,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({username:username, password:password})
+            }).then(response => response.json()).then(json => {
+                localStorage.setItem('user', JSON.stringify(json));
+                dispatch({type:'LOGIN', payload: json}) ;
+                router.push("/");
+            }
+            )
+        } catch(e) {
+            console.log(e);
+        }
+        
+    }
     return (
         <div className='flex-0 m-auto'>
             <form className='border-2 rounded-lg border-orange-300 flex flex-col bg-black content-center h-80 w-64' onSubmit={onSubmit}>
-                <label className="text-red-600 ml-4 flex-1">{response}</label>
+                <label className="text-red-600 ml-4 flex-1"></label>
                 <div className='ml-4  flex-1 flex flex-col' title='input'>
                     <label className='mt-2 pl-2 text-indigo-400'>User name :</label>
                     <input className='mt-2 w-52 rounded-2xl' name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -16,16 +51,18 @@ const Login = () => {
                     <label className='pt-12 pl-2 text-indigo-400'>Password :</label>
                     <input className='mt-2 w-52 rounded-2xl' name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     <button className='w-20 h-8 mt-8 mr-8 rounded-2xl bg-orange-400 self-end' type="submit">Submit</button>
+                    {user &&<span>{user.username}</span>}
                 </div>
 
                 <div className='flex-1 mt-2 flex flex-row justify-evenly items-center'>
                     
                     <a className="h-6 text-orange-600">Don't have an account? </a>
                     <Link href="/auth/signup" className="h-6 text-orange-600 bold ">Sign In!</Link>
+                    
                 </div>
             </form>
         </div>
     );
+    
 }
-
 export default Login;
--- quickly-chat/src/app/auth/signup/page.js
+++ quickly-chat/src/app/auth/signup/page.js
@@ -1,14 +1,49 @@
 "use client"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
-
+import { useRouter } from 'next/navigation'
+import { useAuthContext } from '../../../../hooks/useAuthContext';
 const Signup = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordV, setPasswordV] = useState("");
+
+    const router = useRouter();
+    const {user,dispatch} = useAuthContext();
+    useEffect(() => {if (user != null) {
+        router.push("/")
+        }
+    }, [user])
+    
+    async function onSubmit (event) {
+        event.preventDefault();
+        if (password == passwordV){
+            try {
+                await fetch('http://localhost:3001/api/auth/signup', {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({username:username, password:password})
+                }).then(response => response.json()).then(json => {
+                    localStorage.setItem('user', JSON.stringify(json));
+                    dispatch({type:'LOGIN', payload: json}) 
+                    router.push("/");
+                }
+
+                )
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    }
 
     return (
         <div className='flex-0 m-auto'>
             <form className='border-2 rounded-lg border-orange-300 flex flex-col bg-black content-center h-92 w-64' onSubmit={onSubmit}>
                 <div className='ml-4 mt-4 flex-1 flex flex-col' title='input'>
-                    <label className="text-red-600">{response}</label>
+                    <label className="text-red-600"></label>
                     <label className='mt-8 pl-2 text-indigo-400'>User name :</label>
                     <input className='mt-2 w-52 rounded-2xl' name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         
