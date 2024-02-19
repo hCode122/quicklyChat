6a06a9f2213665d3da23e9fd23197f6a8846f3e7
hCode122 <hCode122@protonmail.me>
2024-02-19 19:27:48 +0300

    updated the main route to /api/data/

--- server/app.js
+++ server/app.js
@@ -32,8 +32,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/auth/',authRouter);
+app.use('/api/data/', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
