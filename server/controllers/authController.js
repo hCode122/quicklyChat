6e5b5c32469bd378711eb6e3beb3c93dae7d77ab
hCode122 <hCode122@protonmail.me>
2024-02-19 19:24:59 +0300

    created a controller for the /api/auth route responsible for login/signup functions

---
+++ server/controllers/authController.js
@@ -0,0 +1,55 @@
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const User = require("../models/User")
+
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn:'3d'})
+}
+
+exports.login = async (req, res) => {
+    const {username, password} = req.body;
+    console.log(username)
+    try {
+        const user = await User.findOne({name:username});
+
+        if (!user) {
+            throw Error("User doesn't exist")
+        }
+
+        const match = bcrypt.compare(password,user.password)
+
+        if (!match) {
+            throw Error("Password is incorrect")
+        }
+
+        const token = createToken(user.i_id);
+
+        return res.status(200).json({username, token});
+    } catch (error) {
+        return res.status(400).json({error:error.message});
+    }
+}
+
+exports.signup = async (req,res) => {
+    const {username, password} = req.body;
+    try {
+        const exists = await User.findOne({name:username})
+
+        if (exists) {
+            throw Error("User already exists!");
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = await User.create({name:username,password:hash});
+
+        const token = createToken(user._id)
+      
+        return res.status(200).json({username, token})
+
+    } catch (error) {
+        return res.status(400).json({error:error.message});
+    }
+}
