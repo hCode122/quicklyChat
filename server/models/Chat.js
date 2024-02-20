bb4941a281c4e7d0e36ab9764ea0894bee163962
hCode122 <hCode122@protonmail.me>
2024-02-20 18:29:02 +0300

    edited the Chat and Message models to store user id as string

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const ChatSchema = new mongoose.Schema({
-    senderId: {type: Number, required: true},
-    chatId: {type: Number, required: true},
-    recId: {type: Number, required: true},
+    senderId: {type: String, required: true},
+    chatId: {type: String, required: true},
+    recId: {type: String, required: true},
     Messages: {type: Array}
 })
 
--- server/models/Message.js
+++ server/models/Message.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
     text: {type: String, required: true},
-    senderId: {type: Number, required: true},
-    chatId: {type: Number, required: true},
+    senderId: {type: String , required: true},
+    chatId: {type: String, required: true},
     date: {type: Date, required: true}
 })
 
