e1fd3ec2fa4ec9bfc1d56c0d7db934d800dd2d38
hCode122 <hCode122@protonmail.me>
2024-02-21 13:36:19 +0300

    added a very basic design to the main page

--- quickly-chat/src/app/page.js
+++ quickly-chat/src/app/page.js
@@ -1,10 +1,9 @@
 "use client"
-
 import { useEffect, useState } from "react"
 import { useAuthContext } from "../../hooks/useAuthContext"
 import { useRouter } from 'next/navigation'
 import { useLogout } from "../../hooks/useLogout";
-import { headers } from "next/headers";
+import Image from "next/image";
 
 export default function MainUi() {
   const [chats, setChats] = useState([]);
@@ -22,21 +21,47 @@ export default function MainUi() {
     if (user == null) {
       router.push("/auth")
       } else {
-        fetchData(user);
+        
       }
   }, [user])
 
   
   return (
   <div className="h-screen flex flex-col bg-white">
-    <div id="upper" className="bg-blue-700 flex-initial h-20">
-      <svg></svg>
-      <p></p>
+    
+    <div id="upper" className="bg-blue-700 gap-5 grid grid-rows-5 grid-cols-5 flex-initial h-24">
+      
+        <svg className="row-start-1 bg-userPic1 bg-no-repeat h-24" ></svg>
+      
+      <p className="col-start-3  row-start-2  text-2xl  text-orange-400 font-bold">Username</p>
     </div>
+
+    <div className="grid grid-rows1 grid-cols-3 gap-5 text-center">
+      <p className="col-start-1  bg-orange-400 ml-2">
+        Contacts
+      </p>
+      <p className="col-start-2  bg-orange-400">
+        Chats
+      </p>
+      <p className="col-start-3  bg-orange-400 mr-2">
+        Stuff
+      </p>
+    </div>
+
     <div id="main" className="bg-black flex-1">
-    <button onClick={handleClick}>LogOut</button>
+
     </div>
   </div>
   )
 }
 
+const fetchData = async (user, setContacts, setChats) => {
+ 
+  await fetch("http://localhost:3001/api/data",{
+    headers: {
+      "Authorization": "Bearer" + user.token
+    }}).then(response => response.json()).then(data =>
+      console.log(data)
+    )
+  
+}
