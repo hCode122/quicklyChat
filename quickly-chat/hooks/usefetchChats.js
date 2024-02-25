3ffbbb551bb4b84b8cde6e6048779d554009a888
hCode122 <hCode122@protonmail.me>
2024-02-25 20:40:35 +0300

    started working on useFetchChats hook which returns fechChats function to get the user chats from the database

---
+++ quickly-chat/hooks/usefetchChats.js
@@ -0,0 +1,27 @@
+import { useAuthContext } from "./useAuthContext";
+
+export default function useFetchChats() {
+    
+
+    const fetchChats = async (token) => {
+        console.log(token)
+        try {
+            await fetch("http://localhost:3001/api/data/chats", {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    "authenication": "bearer" + token.split('.')[1]
+                },
+            }).then(response => response.json() ).then(
+                data => {console.log(data);}
+            )
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return fetchChats;
+}
+
+   
+
