f9117e0b911dda0bb814f3f1143ae315f64640af
hCode122 <hCode122@protonmail.me>
2024-03-05 14:44:50 +0300

    changed the Chats and Contacts fields types to Object.Id

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
-    Chats: [{type: String }],
-    Contacts: [{ type: String }]
+    Chats: [{ type : mongoose.Schema.Types.ObjectId, ref: 'chat' }],
+    Contacts: [{ type : mongoose.Schema.Types.ObjectId, ref: 'user' }]
 })
 
 module.exports = mongoose.model("user", UserSchema);
