98e6590bda4605c5c80bfa2d893732b3a50f0ba1
hCode122 <hcode122@proton.me>
2024-03-07 14:44:14 +0300

    add an onconnect event to print socket id

--- quickly-chat/hooks/useSocket.js
+++ quickly-chat/hooks/useSocket.js
@@ -2,6 +2,10 @@ import { io } from "socket.io-client"
 
 const connectSocket = () => {
     const socket = io('http://localhost:3003')
+    socket.on("connect", () => {
+        console.log("you connected with id: " + socket.id)
+    })
+    return socket
 }
 
 export default connectSocket;
