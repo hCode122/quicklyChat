f23df0bbb02ec8300056eafc1c6cb1c4627b91b5
hCode122 <hcode122@proton.me>
2024-03-16 09:54:32 +0300

    added a disconnect event and a register emit

--- quickly-chat/hooks/useSocket.js
+++ quickly-chat/hooks/useSocket.js
@@ -1,11 +1,17 @@
 import { io } from "socket.io-client"
 
-const connectSocket = () => {
-    const socket = io('http://localhost:3003')
-    socket.on("connect", () => {
-        console.log("you connected with id: " + socket.id)
-    })
-    return socket
+const connectSocket = (user) => {
+    if (user) {
+        const socket = io('http://localhost:3003')
+        socket.on("connect", () => {
+            console.log('connectiong')
+            socket.emit("register", user.username, socket.id)
+        })
+        socket.on("disconnect", () => {
+            socket.emit("leave",user)
+        })
+        return socket
+    }
 }
 
 export default connectSocket;
