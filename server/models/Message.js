b1874c029c65d2871c11edfdb47caecb13d99b9f
hCode122 <hcode122@proton.me>
2024-03-16 11:13:48 +0300

    changed senderName field type to string

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
     text: {type: String, required: true},
-    senderId: {type: mongoose.Schema.Types.ObjectId, ref: 'user' , required: true},
+    senderName: {type: String , required: true},
     chatId: {type: mongoose.Schema.Types.ObjectId, ref: 'chat', required: true},
     date: {type: Date, required: true}
 })
 
-export const MessageModel = mongoose.model("message", MessageSchema);
+module.exports = mongoose.model("message", MessageSchema);
