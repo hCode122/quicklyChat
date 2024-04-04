eb268bff7f7c047093b242aecdfa1756e0ad107d
hCode122 <hcode122@proton.me>
2024-04-04 06:52:32 +0300

    fixed the style and added a grey background

--- quickly-chat/src/components/AddUsrMnu.jsx
+++ quickly-chat/src/components/AddUsrMnu.jsx
@@ -1,11 +1,28 @@
-import { useAuthContext } from "../../hooks/useAuthContext"
+import React from "react"
 const { data } = require("autoprefixer")
 const { useState, useEffect } = require("react")
 
-const AddUsrMnu = ({user}) => {
+const AddUsrMnu = ({user, setSMen},ref) => {
     
     const [search, setSearch] = useState("")
     const [suggestions, setSug] = useState()
+
+
+    const  addUser = async (contact,user) => {
+        setSMen(null)
+        await fetch("http://localhost:3001/api/data/contacts",{
+            method: "POST",
+            headers: {
+                "Accepets": "Application/json",
+                'Content-Type': 'application/json',
+                "Authorization": "bearer " + user
+            },
+            body: JSON.stringify({toAdd: contact.name})
+        }
+            
+        )
+    }
+
     useEffect(() => {
         if (search.length > 0) {
             fetch("http://localhost:3001/api/data/search", {
@@ -20,30 +37,20 @@ const AddUsrMnu = ({user}) => {
         }
     }, [search])
     return(
-        <div className="absolute top-40 rounded-lg left-16 bg-black bg-opacity-60">
-            <div className="text-md text-white">
-                <input className="rounded-t-lg bg-black outline-none bg-opacity-60" onChange={(e) => setSearch(e.target.value)} value={search} type="text">
+        <div  className="h-screen w-screen absolute bg-black-3  opacity-60">
+        <div ref={ref} className="absolute top-4 h-12 left-12 rounded-lg  bg-white">
+            <div className="text-md text-orange-700">
+                <input className="rounded-lg h-12 p-2 outline-none" onChange={(e) => setSearch(e.target.value)} value={search} type="text">
                 </input>
                 {suggestions && suggestions.map((contact, index) => {
-                    return <div onClick={() => addUser(contact, user)} className="clickable p-1">{contact.name}</div>
+                    return <div onClick={() => addUser(contact, user)} className="clickable bg-black p-1">{contact.name}</div>
                 })}
             </div>
+        </div>
         </div>
     )
 }
 
-const  addUser = async (contact,user) => {
-    await fetch("http://localhost:3001/api/data/contacts",{
-        method: "POST",
-        headers: {
-            "Accepets": "Application/json",
-            'Content-Type': 'application/json',
-            "Authorization": "bearer " + user
-        },
-        body: JSON.stringify({toAdd: contact.name})
-    }
-        
-    )
-}
 
-export default AddUsrMnu
+
+export default React.forwardRef(AddUsrMnu)
