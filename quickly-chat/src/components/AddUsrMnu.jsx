5acb2083dee389fceeeed8ddd7b7781a13e4fb53
hCode122 <hcode122@proton.me>
2024-04-05 14:47:05 +0300

    moved the menu slightely upwards

--- quickly-chat/src/components/AddUsrMnu.jsx
+++ quickly-chat/src/components/AddUsrMnu.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-const { data } = require("autoprefixer")
 const { useState, useEffect } = require("react")
 
 const AddUsrMnu = ({user, setSMen},ref) => {
@@ -37,7 +36,7 @@ const AddUsrMnu = ({user, setSMen},ref) => {
         }
     }, [search])
     return(
-        <div  className="h-screen w-screen absolute bg-black-3  opacity-60">
+        <div  className="absolute top-4 w-screen bg-black-3  opacity-90">
         <div ref={ref} className="absolute top-4 h-12 left-12 rounded-lg  bg-white">
             <div className="text-md text-orange-700">
                 <input className="rounded-lg h-12 p-2 outline-none" onChange={(e) => setSearch(e.target.value)} value={search} type="text">
