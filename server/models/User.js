1d40322af86f78c7639cf85a8672f2b0f7c981ed
hCode122 <hcode122@proton.me>
2024-04-11 18:30:40 +0300

    added a lastOnline field to the User schema

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const UserSchema = new mongoose.Schema({
     name: {type: String, required: true},
     password: {type: String, required: true},
     Chats: [{ type : mongoose.Schema.Types.ObjectId, ref: 'chat' }],
-    Contacts: [{ type : mongoose.Schema.Types.ObjectId, ref: 'user' }]
+    Contacts: [{ type : mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    lastOnline: {type: Date}
 })
 
 module.exports = mongoose.model("user", UserSchema);
