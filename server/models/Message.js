9262fa94d5cb4e3cb8ea5be6c06f8effd1f3e932
hCode122 <hcode122@proton.me>
2024-04-15 21:55:31 +0300

    add a 'read' field.

--- server/models/Message.js
+++ server/models/Message.js
@@ -4,7 +4,8 @@ const MessageSchema = new mongoose.Schema({
     text: {type: String, required: true},
     senderName: {type: String , required: true},
     chatId: {type: mongoose.Schema.Types.ObjectId, ref: 'chat', required: true},
-    date: {type: Date, required: true}
+    date: {type: Date, required: true},
+    read: {type:Boolean, required: true}
 })
 
 module.exports = mongoose.model("message", MessageSchema);
