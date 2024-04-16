bb006771a79c387ae7eac6041b9a4c1e1d051a4b
hCode122 <hcode122@proton.me>
2024-04-16 21:46:25 +0300

    added a socket receive event listener
    - helps to update chats live
    
    - helps display number of unread messages

--- quickly-chat/src/app/page.js
+++ quickly-chat/src/app/page.js
@@ -10,7 +10,7 @@ import ContactCard from "../components/ContactCard";
 import ChatCard from "../components/ChatCard";
 import MainComp from "../components/MainComp";
 import { useTargetContext } from "../../hooks/useTargetContext";
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function MainUi() {
   const [selected, setUi] = useState("chats")
@@ -20,42 +20,46 @@ export default function MainUi() {
   
   const [contacts, setContacts] = useState([])  
   const [chats, setChats] = useState()  
-
-  
+  const [connected, setConnected] = useState(false)
+  const [update, setUpdate] = useState(0)
   const {user} = useAuthContext();
-  const {setSoc} = useTargetContext()
+  const [socket,setSoc] = useState()
   useEffect(() => {
     if (user) {
       setUsr(user)
-      const socket = io('http://localhost:3003')
 
-      const connect = () => {
-        console.log('connecting')
-        socket.emit("register", user.username, socket.id)
-      }
+      
+      
+        setConnected(true)
+        const socket = io('http://localhost:3003')
+        setConnected(true)
+        const connect = () => {
+          socket.emit("register", user.username, socket.id)
+        }
 
-      const disconnect = () => {
-        socket.emit("leave",user.token)
-      }
-      socket.on("connect", connect)
-      socket.on("disconnect", disconnect)
-
+        const disconnect = () => {
+          socket.emit("leave",user.token)
+        }
+        socket.on("connect", connect)
+        socket.on("disconnect", disconnect)
+    
       setSoc(socket)
-
+      
       return () => {
         socket.off('connect', connect);
         socket.off('disconnect', disconnect);
       }
-
+      
       } else {
         router.push("/auth")
     }
       
   },[])
   
-
   const fetchContacts = useFetchContacts()
   const fetchChats = useFetchChats();
+
+  
   useEffect(() => {
     const callFetch = async() => {
       if (loggedUsr.token != null) {
@@ -64,26 +68,44 @@ export default function MainUi() {
         const fetchedChats = await fetchChats(loggedUsr.token);
         fetchedContacts && setContacts(fetchedContacts);
         fetchedChats && setChats(fetchedChats);
-        
+        console.log(fetchedChats)
         setLoading(false)
       }
-
     } 
     callFetch()
   },[loggedUsr])
+
+  useEffect(() => {
+    if (socket) {
+      const receive = (message) => {
+        let currChats = {...chats}
+        let sender = message.senderName
+        currChats[sender].unreadCount =  currChats[sender].unreadCount + 1
+        currChats[sender].lastM = message
+        currChats[sender].key = uuidv4()
+        console.log(currChats)
+        setChats(currChats)
+       
+      }
+      socket.once("receive-message", receive)
+    }
+  },[chats])
+
   
   return (    
     <MainComp selected={selected} setUi={setUi} user={loggedUsr.token}>
       {
-        contacts[0] != null && selected=="contacts" ? contacts.map((el, index) => {
+        contacts && selected=="contacts" ? contacts.map((el, index) => {
+          
           return(
             <ContactCard user={user} name={el.name}/>
           )
-        }) : chats != undefined && selected=="chats" ? chats.chatData.map((el, index) => {
+        }) : chats && selected=="chats" ? Object.keys(chats).map((el, index) => {
+          console.log(el)
           return(
-            <ChatCard key={el._id} name={el.recName} lastM={chats.lastMs[index]}/>
+            <ChatCard key={chats[el].key} name={el} lastM={chats[el].lastM} count={chats[el].unreadCount}/>
           )
-        }) : selected=="groubs" ?  
+        }) : selected=="groups" ?  
           (
             <p>Coming Soon...</p>
           )
