a9aaf4dd50460845c483692c4b0dd0f89a9c9588
hCode122 <hcode122@proton.me>
2024-04-19 21:31:25 +0300

    edited the unread count and date divs styles

--- quickly-chat/src/components/ChatCard.js
+++ quickly-chat/src/components/ChatCard.js
@@ -16,11 +16,13 @@ const ChatCard = ({name, lastM, count}) => {
         <div className="flex text-black-2 pt-1 flex-1 h-16 flex-col">
             <div className="flex flex-row gap-4 justify-between ">
                 <div className="font-bold text-xl flex-1">{name}</div>
-                <div className="pt-1 mr-3 text-sm w-8"> {time}</div>
+                <div className="pt-1 mr-2 text-sm w-8"> {time}</div>
             </div>
             <div className="overflow-hidden flex gap-2 text flex-1"> 
                 <p className="flex-1 h-6">{lastM ? lastM.text : ""}</p> 
-                {count ? <p className="ml-auto mr-2 text-center rounded-full w-6 h-6 text-orange-500 bg-black-2
+                {count ? 
+                <p className="ml-auto mr-2 text-center rounded-full w-8 h-6
+                text-orange-500 bg-black-2 text-xs pt-1
                 ">{count}</p> : ""}
             </div>
             
